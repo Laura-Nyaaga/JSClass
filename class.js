da482b1926da41925781fa13dc017b80d909debd
Laura-Nyaaga <nyaagaaluoch@gmail.com>
2024-04-26 09:16:59 +0300

    add file

---
+++ class.js
@@ -0,0 +1,37 @@
+// 1) Create a Car class or function constructor that has the following properties: a. make (string): The make of the car, e.g., "Toyota". 
+// b. model (string): The model of the car, e.g., "Camry". 
+// c. year (number): The year the car was manufactured, e.g., 2020. 
+// d. isAvailable (boolean): Indicates if the car is currently available for rent. 
+
+class Car {
+    constructor(make, model, year, isAvailable) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.isAvailable = isAvailable;
+    }
+    toggleAvailability() {
+        this.isAvailable = !this.isAvailable;
+    }
+}
+const jeep = new Car('Jeep', 'SUV', 2017, 'true')
+console.log({jeep});
+
+// Create a Rental class or function constructor that has the following properties: ● car (Car object): The car that has been rented. 
+// ● renterName (string): The name of the person who rented the car. ● rentalStartDate (Date object): The start date of the rental period. ● rentalEndDate (Date object): The end date of the rental period. 
+
+class Rental {
+    constructor(car, renterName, rentalStartDate, rentalEndDate) {
+        this.car = car;
+        this.renterName = renterName;
+        this.rentalStartDate = new Date(rentalStartDate);
+        this.rentalEndDate = new Date(rentalEndDate);
+    }
+    calculateRentalDuration() {
+        const  differenceInMilliseconds = this.rentalEndDate - this.rentalStartDate;
+        const differenceInDays = differenceInMilliseconds / (1000 * 60 * 60 * 24);
+        return differenceInDays;
+    }
+}
+const uber = new Rental ('Audi', 'Maryann', "2024-4-12", "2024-6-6")
+console.log({uber});
